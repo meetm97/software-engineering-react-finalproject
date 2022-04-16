afcc5364e00d5a95751ab005666ae143af5c4ef2
meet mehta <meetmehta10@gmail.com>
2022-04-16 13:22:18 -0400

    big fix

--- src/components/bookmarks/index.js
+++ src/components/bookmarks/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {React, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import * as service from "../../services/bookmark-service"
 import * as security_service from "../../services/auth-service"
