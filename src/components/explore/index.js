b0a49a29b2d8b16429a92ec98847651cd4a87357
nandakishore2610 <kasula.n@northeastern.edu>
2022-04-16 10:52:52 -0400

    Explore screen

--- src/components/explore/index.js
+++ src/components/explore/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-const Explore = () => {
-  return(
-    <h1>Explore Screen</h1>
-  );
-};
-export default Explore;
+import NavigationSidebar from "../NavigationSidebar";
+import ExploreComponent from "./ExploreComponent";
+import WhoToFollowList from "../WhoToFollowList";
+import './explore.css';
+
+const ExploreScreen = () => {
+    return (
+        <div className="container">
+            <div className="row mt-2">
+                <div className="col-xs-10 col-sm-10 col-md-10 col-lg-7 col-xl-6 col-xxl-6 mt-4"
+                     style={{"position" : "relative"}}>
+                     <ExploreComponent  />
+                </div>
+                <div
+                    className="d-xxl-block d-xl-block d-lg-block d-md-none d-sm-none d-xs-none col-lg-4 col-xl-4 mt-4">
+                    <WhoToFollowList/>
+                </div>
+            </div>
+        </div>
+
+); }
+
+export default ExploreScreen;
